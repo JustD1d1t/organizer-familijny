44d0a991910ae544da7c273845f819523fa86923
JustD1d1t <starzyk.dawid.0826@gmail.com>
2024-08-31 18:10:34 +0200

    deep link part 2

--- plugins/file-jsevent-ex.js
+++ plugins/file-jsevent-ex.js
@@ -3,7 +3,7 @@ import { App } from "@capacitor/app"
 App.addListener("appUrlOpen", (event) => {
     const url = event.url
 
-    if (url.includes("foapp://family")) {
+    if (url.includes("https://family-organizer.com")) {
         window.location.href = "/verify-email"
     }
 })
