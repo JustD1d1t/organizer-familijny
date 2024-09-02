26ade0568af59392a643b6b2182867cf0019e51e
JustD1d1t <starzyk.dawid.0826@gmail.com>
2024-09-02 20:14:36 +0200

    allow register

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -2,17 +2,21 @@ export default defineNuxtRouteMiddleware((to, from) => {
     const uid = localStorage.getItem("uid")
     const emailVerified = localStorage.getItem("emailVerified")
 
-    // Jeśli uid jest niezdefiniowane lub puste, przekieruj do /login
+    // Jeśli uid jest niezdefiniowane lub puste
     if (!uid || uid === "undefined") {
+        // Pozwól przejść do /register
+        if (to.path === "/register") {
+            return // Brak potrzeby przekierowania
+        }
+
+        // Przekieruj do /login, jeśli użytkownik próbuje przejść gdzie indziej
         if (to.path !== "/login") {
-            console.log("Redirecting to /login")
             return navigateTo("/login")
         }
     } else {
         // Jeśli użytkownik jest zalogowany
         if (emailVerified === "false") {
             if (to.path !== "/verify-email") {
-                console.log("Redirecting to /verify-email")
                 return navigateTo("/verify-email")
             }
         } else {
